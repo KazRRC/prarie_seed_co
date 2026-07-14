0efb7b7c067b1227be18c3846dffbb1da32a4c35
KazRRC <gududec2@academic.rrc.ca>
2026-07-14 12:08:59 -0500

    added relationships to product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,21 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :description, presence: true
+
+  validates :price,
+            presence: true,
+            numericality: {
+              greater_than: 0
+            }
+
+  validates :stock_quantity,
+            presence: true,
+            numericality: {
+              only_integer: true,
+              greater_than_or_equal_to: 0
+            }
+
+  validates :image_url, presence: true
 end
